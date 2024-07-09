0d2b30e8c1af67c9e6087e9ebb5f927a167df5be
Suchana Nag <99524477+SuchanaNag002@users.noreply.github.com>
2024-07-09 10:37:29 +0530

    Added userController file to manage user functions

---
+++ index.js
@@ -0,0 +1,28 @@
+// Importing necessary modules
+const express = require("express");
+const mongoose = require("mongoose");
+const { mongoURI } = require("./config");
+
+// Initializing the Express application
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Connecting to MongoDB using mongoose
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Routes for user-related requests
+app.use("/api/users", require("./routes/userRoutes"));
+
+// Routes for admin-related requests
+app.use("/api/admin", require("./routes/adminRoutes"));
+
+// Starting the server and listening on the specified port
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
